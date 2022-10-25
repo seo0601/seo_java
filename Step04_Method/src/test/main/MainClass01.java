362c3808aa865f301e191ddfe14b9ff2676d25ab
Seo dong min <sdm067@nave.com>
2022-10-25 12:23:38 +0900

    Step04_Method 프로젝트 추가함

---
+++ Step04_Method/src/test/main/MainClass01.java
@@ -0,0 +1,18 @@
+package test.main;
+
+import test.mypac.MyObject;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		
+		/* MyObject 객체를 하나만 생성해서 그 객체가 가지고 있는
+		 * walk(), getNumber(), getGreeting() 메소드를 차례대로 호출해 보세요.
+		 * 단, 메소드가 어떤 data를 리턴하면 해당 데이터를 지역 변수에 담으세요.
+		 */
+		
+		MyObject mo = new MyObject();
+		mo.walk();
+		int num = mo.getNumber();
+		String str = mo.getGreeting();
+	}
+}
