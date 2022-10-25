13657fb13906b053e7eccd609eeb1e8d14645006
Seo dong min <sdm067@nave.com>
2022-10-25 11:04:34 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,22 @@
+// package는 이 클래스가 속해 있는 패키지명을 밝혀 준다.
+package test.main;
+
+/*
+ * 여러 줄 주석입니다.
+ * 하나
+ * 두울
+ * 세엣
+ * */
+
+// 공개된(모든 패키지에서 사용할 수 있는) 클래스, 클래스명은 MainClass2
+public class MainClass2 {// 클래스의 시작
+	
+	//한 줄 주석입니다.
+	/*
+	 *  메소드명 : main 
+	 *  run 했을때 실행이 되는 아주 특별한 메소드
+	 */
+	public static void main(String[] args) {
+		
+	}
+}//클래스의 끝
