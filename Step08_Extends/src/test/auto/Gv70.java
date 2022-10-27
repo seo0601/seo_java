45adad6a262639ef4ae0ed119d051e3f5cbbc479
Seo dong min <sdm067@nave.com>
2022-10-27 16:43:55 +0900

    Car 연습

---
+++ Step08_Extends/src/test/auto/Gv70.java
@@ -0,0 +1,15 @@
+package test.auto;
+
+public class Gv70 extends Car{
+
+	public Gv70(Engine engine) {
+		super(engine);
+		
+	}
+	public void clark() {
+		System.out.println("클락션을 울려요");
+	}
+	public void Break() {
+		System.out.println("멈춰요");
+	}
+}
---
+++ Step08_Extends/src/test/main/TestClass4.java
@@ -0,0 +1,10 @@
+package test.main;
+
+import test.mypac.Foods;
+import test.mypac.Ramyeon;
+
+public class TestClass4 {
+	public static void main(String[] args) {
+	
+	}
+}
