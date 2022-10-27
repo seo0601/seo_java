cbf01415a7073f5efdcc928d827d8c4ee50081b7
Seo dong min <sdm067@nave.com>
2022-10-27 16:51:41 +0900

    Step08 프로젝트에 static 메소드 예제 추가

---
+++ Step08_Extends/src/test/main/MainClass08.java
@@ -0,0 +1,13 @@
+package test.main;
+
+import test.util.MyUtil;
+
+public class MainClass08 {
+	public static void main(String[] args) {
+		
+		// 상속 받은 BaseUtil 클래스에 정의된 static 메소드도 모두 사용이 가능하다.
+		MyUtil.send();
+		MyUtil.clear();
+		MyUtil.light();
+	}
+}
